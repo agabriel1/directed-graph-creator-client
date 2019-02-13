8c2743d949f439d0a0a61141bed0309721b1874a
agabriel1 <43051089+agabriel1@users.noreply.github.com>
2019-02-13 00:24:48 -0500

    Update graph-creator.js

--- graph-creator.js
+++ graph-creator.js
@@ -549,27 +549,49 @@ document.onload = (function(d3, saveAs, Blob, undefined){
 //-----------------------------------------------------------------------------------------------------------------------
     newGs.append("circle")
       //.attr("r", 150 ); //String(consts.nodeRadius)
+    
      //{.attr("x", 10)
      //.attr("y", 10)
     //.attr("width", 100)
     //.attr("height", 50)    }
+    
  //[var c = document.getElementById("myCanvas");
 //var ctx = c.getContext("2d");
 //ctx.fillRect(20, 20, 150, 100); ]
-    // referance the canvas element
-    var canvas=document.getElementById("canvas");
+    
+    //{[ referance the canvas element
+    //var canvas=document.getElementById("canvas");
     //create a canvas context
-    var ctx=canvas.getContext("2d");
+   // var ctx=canvas.getContext("2d");
     //context's fill and stroke styles
-    ctx.fillStyle="skyblue";
-ctx.strokeStyle="lightgray";
-ctx.lineWidth=3;
+    //ctx.fillStyle="skyblue";
+//ctx.strokeStyle="lightgray";
+//ctx.lineWidth=3;
     // set up variables to hold the mouse starting X/Y
 // when the user drags the mouse
-var startX;
-var startY;
+//var startX;
+//var startY;
     // set up a variable to determine whether to  draw a square or a rectangle
-  var modeName="square";
+  //var modeName="square"; }]
+    
+    var svg = d3.select("body")
+	.append("svg")
+	.attr("width", 400)
+	.attr("height", 200);
+	
+var data = [{x1: 20, x2: 60, y1: 30, y2: 50},
+//{x1: 50, x2: 80, y1: 100, y2: 150},
+//{x1: 200, x2: 400, y1: 10, y2: 100}];
+
+var rects = svg.selectAll("foo")
+	.data(data)
+	.enter()
+	.append("rect")
+	.attr("x", d=> d.x1)
+	.attr("y", d=> d.y1)
+	.attr("width", d=> d.x2 - d.x1)
+	.attr("height", d=> d.y2 - d.y1)
+	.attr("fill", "teal");
 //------------------------------------------------------------------------------------------------------------------------    
 
     newGs.each(function(d){
